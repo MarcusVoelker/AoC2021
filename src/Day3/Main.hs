61a8cf00eeb3c8499e307a8d4197ee3e06d971c5
Marcus Völker <voelker@embedded.rwth-aachen.de>
2021-12-03 09:29:42 +0100

    unreadably shortened Day 3

--- src/Day3/Main.hs
+++ src/Day3/Main.hs
@@ -1,14 +1,13 @@
 module Main where
 
+import Control.Lens
+import Control.Monad
 import Data.Char
 import Data.List
 import Harness
 
 digit :: Char -> Int
 digit x = ord x - ord '0'
-
-bdigit :: Char -> Bool
-bdigit x = x == '1'
 
 bval :: Bool -> Int
 bval True = 1
@@ -17,34 +16,26 @@ bval _ = 0
 bnum :: [Bool] -> Int
 bnum = foldl (\x y -> x * 2 + bval y) 0
 
+num :: [Int] -> Int
+num = foldl (\x y -> x * 2 + y) 0
+
 p1 :: String -> Int
 p1 s =
-  let ss = transpose $ map (map bdigit) $ lines s
-      zs = map (length . filter not) ss
-      os = map (length . filter id) ss
+  let ss = transpose $ map (map digit) $ lines s
+      os = map sum ss
+      zs = map (length (head ss) -) os
       gs = zipWith (<) zs os
       es = map not gs
    in bnum gs * bnum es
 
-lfilter :: [[Bool]] -> Bool -> Int -> Bool -> [Bool]
-lfilter ss mc idx def =
-  let bs = map (!! idx) ss
-      zs = length $ filter not bs
-      os = length $ filter id bs
-      ss'
-        | zs == os = filter ((== def) . (!! idx)) ss
-        | (zs > os) == mc = filter (not . (!! idx)) ss
-        | otherwise = filter (!! idx) ss
-   in if length ss' == 1 then head ss' else lfilter ss' mc (idx + 1) def
+lfilter :: [[Int]] -> Int -> Int -> [Int]
+lfilter [x] _ _ = x
+lfilter ss idx def = lfilter (filter =<< (. ((def ==) . (!! idx))) . (/=) . liftM2 (>) length ((2 *) . sum . map (!! idx)) $ ss) (idx + 1) def
 
 p2 :: String -> Int
 p2 s =
-  let ss = map (map bdigit) $ lines s
-      os = lfilter ss True 0 True
-      cs = lfilter ss False 0 False
-      or = bnum os
-      cr = bnum cs
-   in or * cr
+  let ss = map (map digit) $ lines s
+   in (0, 1) & both %~ (num . lfilter ss 0) & uncurry (*)
 
 main :: IO ()
 main = run 3 p2
