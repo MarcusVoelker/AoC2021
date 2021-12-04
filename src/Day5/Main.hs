916a1d978969bffd873fca5a546f9c1f9707cbe1
Marcus Völker <voelker@embedded.rwth-aachen.de>
2021-12-04 14:15:44 +0100

    Day 5 prep

---
+++ src/Day5/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TupleSections #-}
+
+module Day5.Main where
+
+import Control.Monad
+import Data.List
+import Data.Ord
+import Harness
+
+p1 :: String -> String
+p1 s = s
+
+p2 :: String -> String
+p2 s = s
+
+main :: Bool -> IO ()
+main b = run 5 b p1 p2
--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,10 @@ import qualified Day1.Main as D1
 import qualified Day2.Main as D2
 import qualified Day3.Main as D3
 import qualified Day4.Main as D4
+import qualified Day5.Main as D5
 
 ms :: [Bool -> IO ()]
-ms = [D1.main, D2.main, D3.main, D4.main]
+ms = [D1.main, D2.main, D3.main, D4.main, D5.main]
 
 main :: IO ()
 main = do
