96c0df127eaa4dcff201b59f8e0ef3f4e0800acc
Marcus Völker <voelker@embedded.rwth-aachen.de>
2021-12-10 08:27:40 +0100

    Day 10

---
+++ src/Day10/Main.hs
@@ -0,0 +1,93 @@
+module Day10.Main where
+
+import Control.Applicative
+import Control.Monad.Trans.State
+import Data.Either
+import Data.List
+import Harness
+
+scoreCorr :: Char -> Int
+scoreCorr ')' = 3
+scoreCorr ']' = 57
+scoreCorr '}' = 1197
+scoreCorr '>' = 25137
+
+scoreIncm :: Char -> Int
+scoreIncm ')' = 1
+scoreIncm ']' = 2
+scoreIncm '}' = 3
+scoreIncm '>' = 4
+
+pair :: Char -> Char
+pair '(' = ')'
+pair '[' = ']'
+pair '{' = '}'
+pair '<' = '>'
+
+isOpen :: Char -> Bool
+isOpen c = c `elem` "([{<"
+
+whileA :: (Monad m) => m Bool -> m a -> m [a]
+whileA cond body = do
+  c <- cond
+  if c
+    then do
+      a <- body
+      (a :) <$> whileA cond body
+    else return []
+
+data Res = Corr Int | Incm Int | OK deriving (Show)
+
+fold :: [Res] -> Res
+fold [] = OK
+fold (Corr x : _) = Corr x
+fold [Incm x] = Incm x
+fold (OK : xs) = fold xs
+fold (Incm x : xs) = fold xs
+
+lineScore :: State String Res
+lineScore = do
+  code <- get
+  case code of
+    [] -> put "" >> return (Incm 0)
+    [c] -> put "" >> return (if isOpen c then Incm (scoreIncm (pair c)) else Corr (scoreCorr c))
+    (c : cs) -> do
+      put cs
+      sc <- fold <$> whileA (get >>= (\s -> return $ not (null s) && isOpen (head s))) lineScore
+      rest <- do
+        code' <- get
+        if null code'
+          then return $ Incm (scoreIncm $ pair c)
+          else do
+            put (tail code')
+            if pair c == head code'
+              then return OK
+              else return $ Corr $ scoreCorr (head code')
+      return $ case (sc, rest) of
+        (Corr x, _) -> Corr x
+        (_, Corr y) -> Corr y
+        (Incm x, OK) -> Incm x
+        (OK, Incm y) -> Incm y
+        (Incm x, Incm y) -> Incm (x * 5 + y)
+        _ -> OK
+
+corr :: Res -> Int
+corr (Corr x) = x
+corr _ = 0
+
+incm :: Res -> Int
+incm (Incm x) = x
+incm _ = 0
+
+median :: [a] -> a
+median [x] = x
+median xs = median (tail (init xs))
+
+p1 :: String -> Int
+p1 s = sum $ map corr $ map (evalState lineScore) $ lines s
+
+p2 :: String -> Int
+p2 s = median $ nub $ sort $ map incm $ map (evalState lineScore) $ lines s
+
+main :: Bool -> IO ()
+main b = run 10 b p1 p2
--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
 import qualified Day1.Main as D1
+import qualified Day10.Main as D10
 import qualified Day2.Main as D2
 import qualified Day3.Main as D3
 import qualified Day4.Main as D4
 import qualified Day5.Main as D5
 import qualified Day6.Main as D6
 import qualified Day7.Main as D7
-import qualified Day8.Main as D7
+import qualified Day8.Main as D8
+import qualified Day9.Main as D9
 
 ms :: [Bool -> IO ()]
-ms = [D1.main, D2.main, D3.main, D4.main, D5.main, D6.main, D7.Main, D8.Main]
+ms = [D1.main, D2.main, D3.main, D4.main, D5.main, D6.main, D7.Main, D8.Main, D9.Main, D10.Main]
 
 main :: IO ()
 main = do
